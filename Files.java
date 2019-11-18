4f0cbf5ec558826e561afca38c73821414ccc49f
PizzaDude007 <42350876+PizzaDude007@users.noreply.github.com>
2019-11-18 17:31:04 -0600

    Add files via upload

--- Files.java
+++ Files.java
@@ -3,15 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectopo;
-import java.io.*;
-import java.util.*;
+package proyectopoo;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.File;
 
 /**
  *
- * @author Pizza Dude
+ * @author essmo
  */
 public class Files {
+    Map<String, Desarrollador> usuarios = new HashMap<>();
+    Map<String, Desarrollador> usuariosR = null;
+    File file = new File("users.ser");
+    public void registrar(Desarrollador usuario){
+        if(!file.exists()){
+            usuarios.put(usuario.getUsuario(), usuario);
+            saveMap("users.ser",usuarios);
+        }else{
+            usuariosR = getMap("users.ser");
+            usuariosR.put(usuario.getUsuario(), usuario);
+            saveMap("users.ser", usuariosR);
+        }
+    }
+    public boolean iniciarSesion(String usuario, String password){
+        boolean i = false;
+        if(file.exists()){
+            usuariosR = getMap("users.ser");
+            if(usuariosR.containsKey(usuario)){
+                Desarrollador user = usuariosR.get(usuario);
+                if(user.getPassword().equals(password)){
+                    i = true;
+                }
+            }
+        }
+        return i;
+    }
     public void saveMap(String nombre, Map hmap){
         try{
             FileOutputStream fOut = new FileOutputStream(nombre);
--- ProyectoPOO.java
+++ ProyectoPOO.java
@@ -3,45 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectopo;
+package proyectopoo;
 
-import java.util.*;
-import javax.swing.JFrame;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  *
- * @author Pizza Dude
+ * @author essmo
  */
 public class ProyectoPOO {
-    public static void main(String[] args) {
-        Map<String,Desarrollador> usuarios = new HashMap();
-        Map<String,Tarea> tareas = new HashMap();
-        Files f = new Files();
-        
-        f.saveMap("usuarios.ser", usuarios);
-        
-        LiderProyecto lider = new LiderProyecto("Juan",new char[]{1,2,3,4,5});
-        Desarrollador d1 = new Desarrollador("Manuel",new char[]{1,2,3,4,5});
-        
-        usuarios.put("Juan", lider);
-        usuarios.put("Manuel", d1);
-        
-        Iterator us = usuarios.entrySet().iterator();
-        while(us.hasNext())
-            System.out.println(us.next());
-        
-        Tarea t1 = new Tarea(1,lider,"Tarea 1","Descripcion ...",(HashMap)usuarios);
-        
-        f.saveMap("usuarios.ser", usuarios);
-        
-        
-        Iterator it = f.getMap("usuarios.ser").entrySet().iterator();
-        while(it.hasNext())
-            System.out.println(it.next());
-        
-        
-        
-        //Interfaz aplicacion = new Interfaz();
-        //System.out.println(aplicacion.getUsuario());
-        //aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+   
 }
